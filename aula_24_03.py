78186418ebc2af7e5a61743e2b9184c22d2a60cf
Thainná Santana <127801891+thainnasantana@users.noreply.github.com>
2023-03-27 21:27:11 -0300

    atualização do exercicio

--- aula_24_03.py
+++ aula_24_03.py
@@ -6,3 +6,20 @@ while nome != "zero" or "0" and nascimento != "zero" or "0":
         print(nome, nascimento)
 
 print("FIM")
+
+>>> from datetime import datetime
+>>> data_nascimento = input('Digite a data de nascimento: ')
+Digite a data de nascimento: 15/02/1982
+>>> datetime.strptime(data_nascimento, '%d/%m/%Y')
+datetime.datetime(1982, 2, 15, 0, 0)
+--
+from datetime import datetime
+from datetime import date
+
+nome = input('Digite o nome: ')
+sobrenome = input('Digite o sobrenome: ')
+nascimento = input('Digite a data de nascimento: ')
+
+idade = date.today().year - datetime.strptime(nascimento, '%d/%m/%Y').year
+
+print(f'Olá! O nome completo é {nome} {sobrenome} e hoje você tem {idade} anos')
